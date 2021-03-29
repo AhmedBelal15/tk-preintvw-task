8071ed7378ffab5c12d379452a538a004f5d6127
ahmedbelal15 <ahmbelal95@gmail.com>
2021-03-29 23:04:12 +0200

    auto open browser on npm start

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const open = require('open');
 
 const app = express()
 
 app.use('/', express.static(__dirname))
+
+setTimeout(()=>open('http://localhost:5050'), 1000)
 
 app.listen(5050, ()=>console.log('app is litening on port 5050'))
